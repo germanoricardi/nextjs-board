f84304513ad8dbfaf54b517bd5686adde4517761
germanoricardi <germanoricardi7@gmail.com>
2022-07-09 11:33:42 -0400

    Adicionado título dinâmico na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head'
 import styles from '../styles/styles.module.scss'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Primeiro projeto com NextJS</h1>
-    </div>
+    <>
+      <Head>
+        <title>Board - Organizando suas tarefas</title>
+      </Head>
+      
+      <div>
+        <h1 className={styles.title}>Primeiro projeto com NextJS</h1>
+      </div>
+    </>
   )
 }
