44798e0de8daf89e91079e16101d6e0f57aad1b8
germanoricardi <germanoricardi7@gmail.com>
2022-07-09 11:24:15 -0400

    Commit com estrutura inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/styles.module.scss'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.title}>Primeiro projeto com NextJS</h1>
+    </div>
+  )
+}
