d81237a95c538517044115e383ca8264eaefbe51
germanoricardi <germanoricardi7@gmail.com>
2022-07-17 10:39:39 -0400

    Criado html da lista de tarefas e botão de apoiar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,30 @@ export default function Home() {
         <title>Board - Organizando suas tarefas</title>
       </Head>
       
-      <div>
-        <h1 className={styles.title}>Primeiro projeto com NextJS</h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <img src="/images/board-user.svg" alt="Ferramenta board" />
+
+        <section className={styles.callToAction}>
+          <h1>Uma ferramenta para seu dia a dia. Escreva, planeje e organize-se.</h1>
+
+          <p>
+            <span>100% gratuito</span> e online.
+          </p>
+        </section>
+
+        <div className={styles.donaters}>
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 1" />
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 2" />
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 3" />
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 4" />
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 5" />
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 6" />
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 7" />
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 8" />
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 9" />
+          <img src="https://sujeitoprogramador.com/steve.png" alt="Usuário 10" />
+        </div>
+      </main>
     </>
   )
 }
